04c00e26800d5b8bec00f131d2650e2f9a5aff89
Marcos Hernández <marcoslhc@gmail.com>
2015-03-23 23:13:06 -0400

    change arrows

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ var chalk = require('chalk'),
 module.exports = {
     success: success('\u2714'),
     error: error('\u2718'),
-    point: info('\u2796'),
+    point: info('\u2794'),
     warning: warning('\u2757'),
     bullet: text('\u2731')
 };
