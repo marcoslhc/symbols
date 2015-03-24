ab2a136876f508750108278dcdbabfb1f9555381
Marcos Hernández <marcoslhc@gmail.com>
2015-03-23 22:47:27 -0400

    Add package.json and index.js

---
+++ index.js
@@ -0,0 +1,14 @@
+var chalk = require('chalk'),
+    success = chalk.green,
+    error = chalk.red,
+    info = chalk.white,
+    warning = chalk.yellow,
+    text = chalk.white;
+
+module.exports = {
+    success: success('\u2714'),
+    error: error('\u2718'),
+    point: info('\u2796'),
+    warning: warning('\u2757'),
+    bullet: text('\u2731')
+};
