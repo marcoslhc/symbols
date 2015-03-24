3597217110ff42ba0284e44ef0b500cc5891e27d
Marcos Hernández <marcoslhc@gmail.com>
2015-03-24 17:05:00 -0400

    Add arrow symbols, change identation for tabs

--- index.js
+++ index.js
@@ -1,14 +1,17 @@
 var chalk = require('chalk'),
-    success = chalk.green,
-    error = chalk.red,
-    info = chalk.white,
-    warning = chalk.yellow,
-    text = chalk.white;
+	success = chalk.green,
+	error = chalk.red,
+	info = chalk.white,
+	warning = chalk.yellow,
+	text = chalk.white,
+	process = chalk.blue;
 
 module.exports = {
-    success: success('\u2714'),
-    error: error('\u2718'),
-    point: info('\u2794'),
-    warning: warning('\u2757'),
-    bullet: text('\u2731')
+	success: success('\u2714'),
+	error: error('\u2718'),
+	point: info('\u2794'),
+	warning: warning('\u2757'),
+	bullet: text('\u2731'),
+	inbound: process('\u10142'),
+	outbound: process('\u11013')
 };
